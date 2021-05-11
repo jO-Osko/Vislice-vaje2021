686b259cc25079ce66fb012ad7f52d868f96497b
Filip Koprivec <koprivec.filip@gmail.com>
2021-05-11 15:38:46 +0200

    Delo z datotekami

--- vislice.py
+++ vislice.py
@@ -1,8 +1,6 @@
 import bottle
 import model
 
-visilice = model.Vislice()
-
 
 @bottle.get("/")
 def index():
@@ -11,16 +9,28 @@ def index():
 
 @bottle.post("/igra/")
 def nova_igra():
+
+    visilice = model.Vislice.preberi_iz_datoteke(
+        model.DATOTEKA_ZA_SHRANJEVANJE
+    )
+
     id_igre = visilice.nova_igra()
     novi_url = f"/igra/{id_igre}/"
+
+    visilice.zapisi_v_datoteko(model.DATOTEKA_ZA_SHRANJEVANJE)
 
     bottle.redirect(novi_url)
 
 
 @bottle.get("/igra/<id_igre:int>/")
 def pokazi_igro(id_igre):
+    visilice = model.Vislice.preberi_iz_datoteke(
+        model.DATOTEKA_ZA_SHRANJEVANJE
+    )
     trenutna_igra, trenutno_stanje = \
         visilice.igre[id_igre]
+
+    visilice.zapisi_v_datoteko(model.DATOTEKA_ZA_SHRANJEVANJE)
 
     return bottle.template("igra.tpl",
         igra=trenutna_igra, stanje=trenutno_stanje
@@ -28,10 +38,13 @@ def pokazi_igro(id_igre):
 
 @bottle.post("/igra/<id_igre:int>/")
 def ugibaj_na_igri(id_igre):
+    visilice = model.Vislice.preberi_iz_datoteke(
+        model.DATOTEKA_ZA_SHRANJEVANJE
+    )
     ugibana = bottle.request.forms["crka"]
     
     visilice.ugibaj(id_igre, ugibana)
-
+    visilice.zapisi_v_datoteko(model.DATOTEKA_ZA_SHRANJEVANJE)
     return bottle.redirect(f"/igra/{id_igre}/")
 
 
